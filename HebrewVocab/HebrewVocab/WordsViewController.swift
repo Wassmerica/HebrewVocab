03e2037d9f867b1cc5061a4dd692da833ee4420e
Michael Wasserman <wmichael@ca.ibm.com>
2016-06-09 20:23:35 -0400

    - english to other language button implemented - spacing for various screen sizes updated

--- HebrewVocab/HebrewVocab/WordsViewController.swift
+++ HebrewVocab/HebrewVocab/WordsViewController.swift
@@ -15,6 +15,7 @@ class WordsViewController: UIViewController {
         tabBarItem = UITabBarItem(title: "Words", image: nil, tag: 0)
     }
     
+    @IBOutlet weak var containerToConstraint: NSLayoutConstraint!
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
@@ -26,17 +27,22 @@ class WordsViewController: UIViewController {
     
     var currentWords:Array<HWord>?
     
+    //MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         currentWords = WordRepository.getCurrentSetOfWords()
         setUpWords()
+        adjustScreenForDevice()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - Word Button Methods
 
     @IBAction func button1Action(sender: UIButton) {
         if let currWords = currentWords, titleLabel = button1.titleLabel{
@@ -98,6 +104,8 @@ class WordsViewController: UIViewController {
         }
     }
     
+    //MARK: - Other Button Methods
+    
     @IBAction func moreWordsAction(sender: UIButton) {
         currentWords = WordRepository.getNextSetOfWords()
         setUpWords()
@@ -109,10 +117,19 @@ class WordsViewController: UIViewController {
     }
     
     @IBAction func langSwitchAction(sender: UIButton) {
-        //check button label - if it == english set "langToUse" to false - change button label
-        //else set "langToUse" to true - change button label to "english"
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if sender.titleLabel?.text == "English" {
+            defaults.setBool(true, forKey: "langToUse")
+            sender.setTitle("Hebrew", forState: UIControlState.Normal)
+            setUpWords()
+        } else {
+            defaults.setBool(false, forKey: "langToUse")
+            sender.setTitle("English", forState: UIControlState.Normal)
+            setUpWords()
+        }
     }
     
+    //MARK: - Helper Methods
     private func useEnglish() -> Bool {
         let defaults = NSUserDefaults.standardUserDefaults()
         let langToUse = defaults.boolForKey("langToUse")
@@ -124,18 +141,24 @@ class WordsViewController: UIViewController {
     }
     
     private func setUpWords() {
-        
         if useEnglish() {
-            
-        }
-        
-        if let currWords = currentWords {
-            button1.setTitle(currWords[0].hebrew, forState: .Normal)
-            button2.setTitle(currWords[1].hebrew, forState: .Normal)
-            button3.setTitle(currWords[2].hebrew, forState: .Normal)
-            button4.setTitle(currWords[3].hebrew, forState: .Normal)
-            button5.setTitle(currWords[4].hebrew, forState: .Normal)
-            button6.setTitle(currWords[5].hebrew, forState: .Normal)
+            if let currWords = currentWords {
+                button1.setTitle(currWords[0].english, forState: .Normal)
+                button2.setTitle(currWords[1].english, forState: .Normal)
+                button3.setTitle(currWords[2].english, forState: .Normal)
+                button4.setTitle(currWords[3].english, forState: .Normal)
+                button5.setTitle(currWords[4].english, forState: .Normal)
+                button6.setTitle(currWords[5].english, forState: .Normal)
+            }
+        } else {
+            if let currWords = currentWords {
+                button1.setTitle(currWords[0].hebrew, forState: .Normal)
+                button2.setTitle(currWords[1].hebrew, forState: .Normal)
+                button3.setTitle(currWords[2].hebrew, forState: .Normal)
+                button4.setTitle(currWords[3].hebrew, forState: .Normal)
+                button5.setTitle(currWords[4].hebrew, forState: .Normal)
+                button6.setTitle(currWords[5].hebrew, forState: .Normal)
+            }
         }
         
         let defaults = NSUserDefaults.standardUserDefaults()
@@ -148,5 +171,28 @@ class WordsViewController: UIViewController {
         navBar.topItem?.title = "Group \(currGroup)"
     }
 
+    private func adjustScreenForDevice() {
+        if UIDevice().userInterfaceIdiom == .Phone {
+            switch UIScreen.mainScreen().nativeBounds.height {
+            case 480:
+                //iPhone Classic
+                containerToConstraint.constant = 20
+            case 960:
+                //iPhone 4 or 4S
+                containerToConstraint.constant = 20
+            case 1136:
+                //iPhone 5 or 5S or 5C
+                containerToConstraint.constant = 20
+            case 1334:
+                //iPhone 6 or 6S
+                containerToConstraint.constant = 60
+            case 2208:
+                //iPhone 6+ or 6S+
+                containerToConstraint.constant = 100
+            default:
+                print("unknown")
+            }
+        }
+    }
 }
 
